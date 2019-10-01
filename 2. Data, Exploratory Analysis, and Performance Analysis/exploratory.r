c8a15437f2d1ff452cd3e74cda2ab66403791369
shakibhasan <nazmusshakib94844@gmail.com>
2019-10-01 21:00:38 +0600

    basic

---
+++ 2. Data, Exploratory Analysis, and Performance Analysis/exploratory.r
@@ -0,0 +1,12 @@
+# import csv file 
+myExpData <- read.csv("~\C:\Users\shakib\Google Drive\github\The-Data-Science-of-Marketing\2. Data, Exploratory Analysis, and Performance Analysis\exploratory-r.csv")
+
+# get a quick snapshot of your data
+
+# shift the names to each row
+
+# review that transformation 
+
+# transform into a matrix 
+
+# generate our heatmap
