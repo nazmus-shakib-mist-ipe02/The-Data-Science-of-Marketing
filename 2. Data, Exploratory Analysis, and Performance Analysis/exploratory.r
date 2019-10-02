a10fba2107c13bf4d5bbd713e533a201936bb214
shakibhasan <nazmusshakib94844@gmail.com>
2019-10-02 19:12:22 +0600

    basic

--- 2. Data, Exploratory Analysis, and Performance Analysis/exploratory.r
+++ 2. Data, Exploratory Analysis, and Performance Analysis/exploratory.r
@@ -1,6 +1,5 @@
 # import csv file 
-myExpData <- read.csv("~\C:\Users\shakib\Google Drive\github\The-Data-Science-of-Marketing\2. Data, Exploratory Analysis, and Performance Analysis\exploratory-r.csv")
-
+myExpData <- read.csv('exploratory-r.csv')
 # get a quick snapshot of your data
 
 # shift the names to each row
